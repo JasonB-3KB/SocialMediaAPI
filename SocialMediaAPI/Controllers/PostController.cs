1401f900f74a6405854e93d01df743d093961232
Jason Bagwill <mr.bagwill@gmail.com>
2021-02-05 09:54:35 -0500

    added Jason's branch

--- SocialMediaAPI/Controllers/PostController.cs
+++ SocialMediaAPI/Controllers/PostController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,40 @@ using System.Web.Http;
 
 namespace SocialMediaAPI.Controllers
 {
+    [Authorize]
     public class PostController : ApiController
     {
+        private PostService CreatePostService()
+        {
+            var postId = Guid.Parse(User.Identity.GetPostId());
+            var postService = new PostService(postId);
+            return postService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+        public IHttpActionResult Post(PostCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Get(int postId)
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPostById(postId);
+            return Ok(post);
+        }
+
     }
 }
