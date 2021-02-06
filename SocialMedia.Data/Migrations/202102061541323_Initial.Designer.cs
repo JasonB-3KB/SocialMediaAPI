c3524fb8bc1e6e1567bd1523d72e2b853dd8e5ad
Jason Bagwill <mr.bagwill@gmail.com>
2021-02-06 11:03:53 -0500

    fully functional

--- SocialMedia.Data/Migrations/202102061541323_Initial.Designer.cs
+++ SocialMedia.Data/Migrations/202102061541323_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace SocialMedia.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202102061306480_initial"; }
+            get { return "202102061541323_Initial"; }
         }
         
         string IMigrationMetadata.Source
